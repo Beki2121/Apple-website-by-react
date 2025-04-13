96e25c87d9920c7d4f883150def546db7f67c721
Beki2121 <berekethabtamu2121@gmail.com>
2025-04-13 21:32:33 +0300

    file uploaded

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./commonResource/css/bootstrap.css";
+import "./commonResource/css/styles.css";
+// import "./commonResource/js/bootstrap.js"
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
